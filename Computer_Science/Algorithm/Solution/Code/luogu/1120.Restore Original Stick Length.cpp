e22dd767522abdc3fc33d570e63349fb87d5f0dc
fang-tech <2819187982@qq.com>
2024-11-26 23:04:09 +0800

    Algorithm: update luogu 1120

--- Computer_Science/Algorithm/Solution/Code/luogu/1120.Restore Original Stick Length.cpp
+++ Computer_Science/Algorithm/Solution/Code/luogu/1120.Restore Original Stick Length.cpp
@@ -8,64 +8,47 @@
 using namespace std;
 int n;
 int a[55];
+vector<int> Div;
 int used[55];
 int sum;
 int flag = 0;
-void dfs(const vector<int>& lengths, int d, int start, int cur_sum, int& counter) {
-    if (counter == sum / n) flag = 1;
-    if (flag) return;
-    if (start == lengths.size()) {
-        flag = 1;
-        return;
-    }
-    if (used[start]) return;
-    cur_sum += lengths[start];
-    if (cur_sum == d) {
-        counter++;
-        used[start] = 1;
-        dfs(lengths, d, start+1, 0, counter);
-        used[start] = 0;
-        counter--;
-    }
-    if (cur_sum < d) {
-        used[start] = 1;
-        for(int i = start+1; i < lengths.size(); i++)
-            dfs(lengths, d, i, cur_sum, counter);
-    }
-    if (cur_sum > d) {
-        return;
-    }
-}
+int d = 0; // 用于存现在的约数
+int cnt;
 
-void Divisor(vector<int>& div, int n) {
-    div.clear();
+void dfs(int start, int cur_sum, int num) {
+    if (flag) return;
+    if (num == cnt+1) {flag = 1; return;}  // 最后合成了需要数量的木棍
+    // 越界问题
+    cur_sum += a[start];
+    if (cur_sum > )
+
+}
+void Divisor(int n) {
+    // 求一个数字的所有约数
+    Div.clear();
     for (int i = 1; i*i < n; i++) {
         if (n%i==0) {
-            div.push_back(i);
-            if (i*i != n) div.push_back(n/i);
+            Div.push_back(i);
+            if (i*i != n) Div.push_back(n/i);
         }
     }
-    sort(div.begin(), div.end());
+    sort(Div.begin(), Div.end());
 }
 
 int main() {
     freopen("input.txt", "r", stdin);
     while (scanf("%d", &n)) {
-        memset(a, 0, sizeof(a));
         sum = 0;
         for (int i = 0; i < n; i++) {cin>>a[i]; sum+=a[i];}
-        vector<int> lengths(a, a + n);
-        sort(lengths.begin(), lengths.end(), greater<int>()); // 将小木棍从大到小排列, 用于优化搜索的顺序
-        vector<int> D;// 用于存约数
+        sort(a, a+n, greater<int>()); // 将小木棍从大到小排列, 用于优化搜索的顺序
         // 求所有的约数
-        Divisor(D, sum);
-        for (int d : D) {
-            if (d >= lengths[0]) {
+        Divisor(sum);
+        for (int i = 0; i < Div.size(); i++) {
+            d = Div[i];
+            if (d >= a[0]) {
                 memset(used, 0, sizeof(used));
-                int counter = 0;
                 flag = 0;
-                dfs(lengths, d, 0, 0, counter);
-                if (counter == sum/d) {cout<<d<<"\n"; break;}
+                cnt = sum / d; // 最后合成的木棍数
             }
         }
     }
