a89d60433e7df9bd926838ec268b61dd48e17377
fang-tech <2819187982@qq.com>
2024-11-26 18:22:25 +0800

    sync for two device

--- Computer_Science/Algorithm/Solution/Code/luogu/1120.Restore Original Stick Length.cpp
+++ Computer_Science/Algorithm/Solution/Code/luogu/1120.Restore Original Stick Length.cpp
@@ -2,23 +2,55 @@
 #include <algorithm>
 #include <numeric>
 #include <cstdio>
+#include <bits/stdc++.h>
 #include <set>
 #include <iostream>
 using namespace std;
 int n;
 int a[55];
-int flag = 0;
 int used[55];
 int sum;
-void dfs(vector<int> lengths, int d) {
-    // 验证通过全排列原来的木棍, 能不能做到将它还原为长度为d的长木棍
-    
-    
+int flag = 0;
+void dfs(const vector<int>& lengths, int d, int start, int cur_sum, int& counter) {
+    if (counter == sum / n) flag = 1;
+    if (flag) return;
+    if (start == lengths.size()) {
+        flag = 1;
+        return;
+    }
+    if (used[start]) return;
+    cur_sum += lengths[start];
+    if (cur_sum == d) {
+        counter++;
+        used[start] = 1;
+        dfs(lengths, d, start+1, 0, counter);
+        used[start] = 0;
+        counter--;
+    }
+    if (cur_sum < d) {
+        used[start] = 1;
+        for(int i = start+1; i < lengths.size(); i++)
+            dfs(lengths, d, i, cur_sum, counter);
+    }
+    if (cur_sum > d) {
+        return;
+    }
+}
+
+void Divisor(vector<int>& div, int n) {
+    div.clear();
+    for (int i = 1; i*i < n; i++) {
+        if (n%i==0) {
+            div.push_back(i);
+            if (i*i != n) div.push_back(n/i);
+        }
+    }
+    sort(div.begin(), div.end());
 }
 
 int main() {
     freopen("input.txt", "r", stdin);
-    while (scanf("%d")) {
+    while (scanf("%d", &n)) {
         memset(a, 0, sizeof(a));
         sum = 0;
         for (int i = 0; i < n; i++) {cin>>a[i]; sum+=a[i];}
@@ -26,20 +58,15 @@ int main() {
         sort(lengths.begin(), lengths.end(), greater<int>()); // 将小木棍从大到小排列, 用于优化搜索的顺序
         vector<int> D;// 用于存约数
         // 求所有的约数
-        for (int i = 1; i * i <= sum; i++) {
-            if (n % i == 0) {
-                D.push_back(i);
-                if (i * i != n) {
-                    D.push_back(n / i);
-                }
-            }
-        }
-        sort(D.begin(), D.end());
-
+        Divisor(D, sum);
         for (int d : D) {
-            flag = 0;
-            dfs(lengths, d);
-            if (flag) {cout<<d<<"\n"; break;}
+            if (d >= lengths[0]) {
+                memset(used, 0, sizeof(used));
+                int counter = 0;
+                flag = 0;
+                dfs(lengths, d, 0, 0, counter);
+                if (counter == sum/d) {cout<<d<<"\n"; break;}
+            }
         }
     }
     return 0;
