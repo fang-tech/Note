4fb8b7bc542d56e79896e46582caee35b10f82e6
Simon <tinayufang978@gmail.com>
2025-03-10 15:32:50 +0800

    Algorithm

--- Computer_Science/Algorithm/EnvironmentTest.cpp
+++ Computer_Science/Algorithm/EnvironmentTest.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
+
+    int dfs(int i) {
+        if (i < 0) return 0;
+        if (i == 0) return 1;
+        return dfs(i - 1) 
+    }
+
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
